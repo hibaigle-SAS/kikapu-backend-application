421e0d9820e8ea343f2258906c3fb06b7ca20cad
Hibaigle <hibaigleadm@gmail.com>
2026-09-19 18:33:37 +0300

    Handle CRU operations for action categories

--- src/modules/action-categories/action-categories.controller.ts
+++ src/modules/action-categories/action-categories.controller.ts
@@ -1,4 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { JwtGuard } from '../auth/guard';
+import { ActionCategoriesService } from './action-categories.service';
+import { ActionCategories } from 'generated/prisma/browser';
 
+@ApiBearerAuth('access-token')
+@UseGuards(JwtGuard)
 @Controller('action-categories')
-export class ActionCategoriesController {}
+export class ActionCategoriesController {
+  constructor(
+    private readonly actionCategoriesService: ActionCategoriesService,
+  ) {}
+
+  @ApiOperation({
+    summary: 'This API fetches action categories',
+  })
+  @Get()
+  get(
+    @Query('page', new ParseIntPipe()) page: number,
+    @Query('limit', new ParseIntPipe()) limit: number,
+  ) {
+    return this.actionCategoriesService.fetch(page, limit);
+  }
+
+  @ApiOperation({
+    summary: 'This API updates action categories',
+  })
+  @Put(':id')
+  update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body(ValidationPipe) dto: ActionCategories,
+  ) {
+    return this.actionCategoriesService.update(id, dto);
+  }
+
+  @ApiOperation({
+    summary: 'This API creates action typesa',
+  })
+  @Post()
+  create(@Body(ValidationPipe) dto: ActionCategories) {
+    return this.actionCategoriesService.create(dto);
+  }
+}
--- src/modules/action-categories/action-categories.service.ts
+++ src/modules/action-categories/action-categories.service.ts
@@ -1,4 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '@/database/database.service';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { ActionCategoryDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client';
 
 @Injectable()
-export class ActionCategoriesService {}
+export class ActionCategoriesService {
+  private readonly logger = new Logger(ActionCategoriesService.name);
+
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async fetch(page: number, limit: number) {
+    const count = await this.databaseService.actionCategories.count();
+    const result = await this.databaseService.actionCategories.findMany({
+      take: limit,
+      skip: (page - 1) * limit,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return {
+      count,
+      data: result,
+    };
+  }
+
+  async create(data: ActionCategoryDto) {
+    try {
+      const result = await this.databaseService.actionCategories.create({
+        data,
+      });
+      return result;
+    } catch (error) {
+      this.logger.error(error);
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error?.code === 'P2002') {
+          if (error.meta?.target[0] === 'name')
+            throw new ConflictException('Nom déjà utilisé');
+        } else {
+          throw new InternalServerErrorException(error);
+        }
+      }
+    }
+  }
+
+  async update(id: string, data: ActionCategoryDto) {
+    const { name, description } = data;
+    try {
+      const result = await this.databaseService.actionTypes.update({
+        data: {
+          ...(name !== undefined ? { name } : {}),
+          ...(description !== undefined ? { description } : {}),
+        },
+        where: { id },
+      });
+      return result;
+    } catch (error) {
+      this.logger.error(error);
+      throw error;
+    }
+  }
+}
